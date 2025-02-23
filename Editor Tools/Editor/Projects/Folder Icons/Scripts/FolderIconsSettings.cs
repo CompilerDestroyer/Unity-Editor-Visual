29db8297114a5a6eedd1f8ab187b3a3a875754b8
Compiler Butcher <alcarinpower@gmail.com>
2025-02-23 05:31:51 +0300

    Revert "Update FolderIconsSettings.cs"
    This reverts commit 3e503cfd5c16059faf44a0e508a768c7eaf70838.

--- Editor Tools/Editor/Projects/Folder Icons/Scripts/FolderIconsSettings.cs
+++ Editor Tools/Editor/Projects/Folder Icons/Scripts/FolderIconsSettings.cs
@@ -9,6 +9,7 @@
     using UnityEditor.UIElements;
     using CompilerButcher.Editor.UIElements;
     using System.Linq;
+    using UnityEngine.AddressableAssets;
     public sealed class FolderIconsSettings
     {
         private static ListView iconSetListView;
